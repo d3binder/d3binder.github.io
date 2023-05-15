0ec13879f53467f5af65d62b536cb3fb1085d986
Jona Wilks <6590932+DarkCloud4040@users.noreply.github.com>
2023-05-15 08:13:45 -0600

    fb
    fb

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,15 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+var checker = document.getElementById('checkme');
+var sendbtn = document.getElementById('sendNewSms');
+// when unchecked or checked, run the function
+checker.onchange = function(){
+if(this.checked){
+   sendbtn.disabled = false;
+} else {
+   sendbtn.disabled = true;
+}
+
+}
